9c67bec8e877e6f162f71d4ef0220cdd379e5238
lucasescouto-ux <lucas.escouto@saipos.com>
2026-05-04 00:55:07 -0300

    Aula #15 - Adicionando operações: isNaN e Array.length

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -1,5 +1,6 @@
 class CalcController {
     constructor() {
+       this._operation = [];
        this._locale = "pt-BR";
        this._displayCalcEl = document.querySelector("#display");
        this._dateCalcEl = document.querySelector("#data");
@@ -24,12 +25,115 @@ class CalcController {
 
     }
 
+    clearAll(){
+        this._operation = [];
+    }
+
+    clearEntry(){
+        this._operation.pop();
+    }
+
+    getLastOperation(){
+        return this._operation[this._operation.length - 1];
+    }
+
+    setlastOperation(value){
+        this._operation[this._operation.length - 1] = value;
+
+    }
+
+    isOperator(value){
+        return (["+", "-", "*", "%", "/"].indexOf(value) > -1);
+    }
+
+    addOperation(value){
+
+        if(isNaN(this.getLastOperation())){
+
+        if(this.isOperator(value)){
+            this._setlastOperation(value);
+
+        } else if(isNaN(value)){
+            console.log(value);
+
+        } else {
+            this._operation.push(value);
+
+        }
+
+        } else {
+           let newValue = this.getLastOperation().toString() + value.toString();
+           this.setlastOperation(parseInt(newValue));
+
+        }
+
+        console.log(this._operation);
+    }
+
+    execBtn(value){
+        switch(value){
+            case "ac":
+                this.clearAll();
+
+                break;
+            case "ce":
+                this.clearEntry();
+
+                break;
+
+            case "soma":
+                this.addOperation("+");
+                break;
+    
+            case "subtracao":
+                this.addOperation("-");
+                break;
+
+            case "divisao":
+                this.addOperation("/"); 
+                break;
+
+            case "multiplicacao":
+                this.addOperation("*");
+                break;
+
+            case "igual":
+                this.addOperation("=");
+                break;
+
+            case "porcento":
+                this.addOperation("%");   
+                break;
+
+            case "ponto":
+                this.addOperation(".");
+                break;
+            
+            case "0":
+            case "1":
+            case "2":
+            case "3":
+            case "4":
+            case "5":
+            case "6":
+            case "7":
+            case "8":
+            case "9":
+                this.addOperation(parseInt(value));
+                break;
+        }
+    }
+
     initButtonsEvents(){
         let buttons = document.querySelectorAll("#buttons > g, #parts > g");
 
         buttons.forEach((btn, index) => {
-            this.addEventListenerAll(btn,"click drag", e => {
-                console.log(btn.className.baseVal.replace("btn-", ""));
+            this.addEventListenerAll(btn, "click drag", e => {
+
+            console.log(btn.className.baseVal.replace("btn-", ""));
+            let textBtn = btn.className.baseVal.replace("btn-", "");
+            this.execBtn(textBtn);
+
             });
 
             this.addEventListenerAll(btn, "mouseover mouseup mousedown", e => {
@@ -37,6 +141,7 @@ class CalcController {
             });
             
         });
+
     }
 
     setDisplayDateTime(){
