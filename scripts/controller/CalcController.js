fb6091e6838423165f702d92b2409611f187d41a
lucasescouto-ux <lucas.escouto@saipos.com>
2026-05-06 00:14:13 -0300

    Aula 26# - Funcionalidade que retorna uma mensagem de "Error" na tela da calculadora caso tenha tentado fazer alguma operação e não tenha conseguido, quando inserido com alguma informação invalida.

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -190,8 +190,15 @@ class CalcController {
     }
 
     getResult(){
-        return eval(this._operation.join(""));
+        try{
+            return eval(this._operation.join(""));
+        } catch(e) {
 
+            setTimeout(() => {
+                this.setError();
+            }, 1);
+            
+        }
     }
 
     calc(){
