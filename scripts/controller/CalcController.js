b8eef2ff6cbf5a61ef847244b575563b63c6eb74
lucasescouto-ux <lucas.escouto@saipos.com>
2026-05-06 00:02:12 -0300

    Aula 25# - Limitando a calculadora em 10 digitos somente.

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -454,6 +454,12 @@ class CalcController {
     }
 
     set displayCalc(value){
+
+        if (value.toString().length > 10) {
+            this.setError();
+            return false;
+        }
+
         this._displayCalcEl.innerHTML = value;
 
     }
